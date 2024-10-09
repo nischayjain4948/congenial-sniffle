7e5443caea4a26762e688fd61bfdee9fb9938a4d
Nischay jain <nischayjain4948@gmail.com>
2024-10-09 20:30:42 +0530

    update in a /user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,22 +3,29 @@ import aron2 from 'argon2'
 
 
 export const createUser = async (req, res) => {
-    const { name, email, password } = req.body;
-    const findEmail = await prisma.user.findUnique({ where: { email: email } });
-    if (findEmail) {
-        return res.status(400).json({ msg: "email already exist please use different email" });
+    try {
+        const { name, email, password } = req.body;
+        const findEmail = await prisma.user.findUnique({ where: { email: email } });
+        if (findEmail) {
+            return res.status(400).json({ msg: "email already exist please use different email" });
+        }
+
+
+        const newUser = await prisma.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password,
+                hash: await aron2.hash(password)
+            }
+        })
+
+        return res.status(200).json({ msg: "user_created", user: newUser })
+
+    }
+    catch (error) {
+        res.status(500).json({ msg: error });
     }
 
-
-    const newUser = await prisma.user.create({
-        data: {
-            name: name,
-            email: email,
-            password: password,
-            hash: await aron2.hash(password)
-        }
-    })
-
-    return res.status(200).json({ msg: "user_created", user: newUser })
 
 }
